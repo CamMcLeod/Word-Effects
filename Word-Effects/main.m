a4581cd5953559eac19fa18728a58ee4c9deaa46
Cam McLeod <cammcleod3@gmail.com>
2019-05-27 12:44:48 -0700

    Initial Commit

---
+++ Word-Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word-Effects
+//
+//  Created by Cameron Mcleod on 2019-05-27.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
